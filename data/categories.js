1949269429e74ca5aaa6673877d879a981742581
Rob <hovhannisiaan@gmail.com>
2024-07-15 20:20:11 +0400

    first commit

---
+++ data/categories.js
@@ -0,0 +1,21 @@
+import kitchenImg from '../public/img/main/kitchen.jpg';
+import barImg from '../public/img/main/bar.jpg';
+import hookahImg from '../public/img/main/hookah.jpg';
+
+export const mainCategories = [
+    {
+        name: 'kitchen',
+        link: '/kitchen',
+        img: kitchenImg,
+    },
+    {
+        name: 'bar',
+        link: '/bar',
+        img: barImg,
+    },
+    {
+        name: 'hookah',
+        link: '/hookah',
+        img: hookahImg,
+    },
+]
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './style.module.scss';
+import Image from "next/image";
+import logoImg from '../../../public/img/logo.png';
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <div className={`${styles.headerContainer} page-container`}>
+                <div className={styles.logoContianer}>
+                    <div className="img-container">
+                        <Image className={styles.logo} src={logoImg} alt="logo" width={57.81} height={61}/>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
